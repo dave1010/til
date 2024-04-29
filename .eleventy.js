7c696017a4d6a0d7699b83a3102df7762a838a1b
Dave Hulbert <dave.hulbert@passengerteam.com>
2024-04-29 19:54:11 +0100

    Clean up

--- .eleventy.js
+++ .eleventy.js
@@ -12,46 +12,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("**/*.png");
     eleventyConfig.addPassthroughCopy("**/*.gif");
 
-
-    // Copy README.md to index.md before the build
-    // eleventyConfig.on('beforeBuild', () => {
-    //     console.log("before");
-    //     const readmePath = path.join(__dirname, 'README.md');
-    //     const indexPath = path.join(__dirname, 'index.md');
-
-    //     if (fs.existsSync(readmePath)) {
-    //         fs.copyFileSync(readmePath, indexPath);
-    //     }
-    // });
-    
-    // Treat README.md as the index page
-    // eleventyConfig.addCollection("homepage", function(collectionApi) {
-    //     return collectionApi.getFilteredByGlob("./README.md").map(item => {
-    //         item.url = "/";  // Set the URL to the site root
-    //         return item;
-    //     });
-    // });
-
-    // eleventyConfig.setUseGitIgnore(false);
-
-    // eleventyConfig.addCollection("nestedPosts", function(collectionApi) {
-    //     let nestedPosts = {};
-    //     collectionApi.getAll().forEach(item => {
-    //         if (item.url !== '/') { // Ignore the homepage
-    //             const segments = item.url.split('/').filter(Boolean);
-    //             if (segments.length > 1) {
-    //                 const [category, page] = segments;
-    //                 if (!nestedPosts[category]) {
-    //                     nestedPosts[category] = [];
-    //                 }
-    //                 console.log(item.templateContent);
-    //                 nestedPosts[category].push({ title: item.data.title, url: item.url });
-    //             }
-    //         }
-    //     });
-    //     return nestedPosts;
-    // });
-
     eleventyConfig.addCollection("nestedPosts", function(collectionApi) {
         let nestedPosts = {};
         collectionApi.getAll().forEach(item => {
@@ -88,8 +48,6 @@ module.exports = function(eleventyConfig) {
             }
         });
 
-
-
         // Convert nestedPosts to an array of {category, posts}
         let categories = Object.keys(nestedPosts).map(category => {
             return { category: category, posts: nestedPosts[category] };
@@ -99,8 +57,6 @@ module.exports = function(eleventyConfig) {
         categories.sort((a, b) => a.category.localeCompare(b.category));
 
         return categories;
-
-        
     });
 
     return {
